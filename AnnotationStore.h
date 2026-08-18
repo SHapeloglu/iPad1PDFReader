cd410d0a5118ba3295d65ed6f2a3a04af5c94584
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 14:39:05 +0300

    Add lightweight annotation editing helpers

--- AnnotationStore.h
+++ AnnotationStore.h
@@ -2,5 +2,7 @@
 @interface AnnotationStore : NSObject
 + (NSArray *)annotationsForPath:(NSString *)path page:(NSUInteger)page;
 + (void)addAnnotation:(NSDictionary *)a path:(NSString *)path page:(NSUInteger)page;
++ (void)replaceAnnotationAtIndex:(NSUInteger)index withAnnotation:(NSDictionary *)a path:(NSString *)path page:(NSUInteger)page;
++ (void)removeAnnotationAtIndex:(NSUInteger)index path:(NSString *)path page:(NSUInteger)page;
 + (void)clearAnnotationsForPath:(NSString *)path page:(NSUInteger)page;
 @end
