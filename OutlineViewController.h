9b005d91539b5ac24bd53bddb514673cbbdceec1
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 16:15:55 +0300

    Make outline navigation actionable

--- OutlineViewController.h
+++ OutlineViewController.h
@@ -1,4 +1,11 @@
 #import <UIKit/UIKit.h>
-@interface OutlineViewController:UITableViewController{NSArray*_items;}
+@protocol OutlineViewControllerDelegate <NSObject>
+- (void)outlineControllerSelectedPage:(NSUInteger)page;
+@end
+@interface OutlineViewController:UITableViewController{
+    NSArray*_items;
+    id<OutlineViewControllerDelegate> _delegate;
+}
+@property(nonatomic,assign) id<OutlineViewControllerDelegate> delegate;
 - (id)initWithItems:(NSArray*)items;
 @end
