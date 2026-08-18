b1e6a87b9a17f3a1c387a702d407993f12bc8568
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 16:16:07 +0300

    Make outline navigation actionable

--- OutlineViewController.m
+++ OutlineViewController.m
@@ -1,7 +1,28 @@
 #import "OutlineViewController.h"
 @implementation OutlineViewController
+@synthesize delegate=_delegate;
 - (id)initWithItems:(NSArray*)i{if((self=[super initWithStyle:UITableViewStylePlain])){_items=[i copy];self.title=@"İçindekiler";}return self;}
 - (NSInteger)tableView:(UITableView*)t numberOfRowsInSection:(NSInteger)s{return [_items count];}
-- (UITableViewCell*)tableView:(UITableView*)t cellForRowAtIndexPath:(NSIndexPath*)i{static NSString*cid=@"o";UITableViewCell*c=[t dequeueReusableCellWithIdentifier:cid];if(!c)c=[[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cid]autorelease];NSDictionary*d=[_items objectAtIndex:i.row];c.indentationLevel=[[d objectForKey:@"depth"]intValue];c.textLabel.text=[d objectForKey:@"title"];return c;}
+- (UITableViewCell*)tableView:(UITableView*)t cellForRowAtIndexPath:(NSIndexPath*)i{
+    static NSString*cid=@"o";
+    UITableViewCell*c=[t dequeueReusableCellWithIdentifier:cid];
+    if(!c)c=[[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cid]autorelease];
+    NSDictionary*d=[_items objectAtIndex:i.row];
+    c.indentationLevel=[[d objectForKey:@"depth"]intValue];
+    c.textLabel.text=[d objectForKey:@"title"];
+    NSNumber *page=[d objectForKey:@"page"];
+    c.detailTextLabel.text=page?[NSString stringWithFormat:@"Sayfa %@",page]:@"Sayfa hedefi çözülemedi";
+    c.accessoryType=page?UITableViewCellAccessoryDisclosureIndicator:UITableViewCellAccessoryNone;
+    c.selectionStyle=page?UITableViewCellSelectionStyleBlue:UITableViewCellSelectionStyleNone;
+    return c;
+}
+- (void)tableView:(UITableView*)t didSelectRowAtIndexPath:(NSIndexPath*)i{
+    NSDictionary*d=[_items objectAtIndex:i.row];
+    NSNumber *page=[d objectForKey:@"page"];
+    if(!page)return;
+    NSUInteger p=[page unsignedIntegerValue];
+    if([_delegate respondsToSelector:@selector(outlineControllerSelectedPage:)])[_delegate outlineControllerSelectedPage:p];
+    [self.navigationController popViewControllerAnimated:YES];
+}
 - (void)dealloc{[_items release];[super dealloc];}
 @end
