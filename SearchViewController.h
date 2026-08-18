47575a161dc405dcafdca79889bc8b2eaf3b6c31
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 16:14:41 +0300

    Add bounded cancellable PDF search

--- SearchViewController.h
+++ SearchViewController.h
@@ -5,10 +5,16 @@
 @end
 @interface SearchViewController : UITableViewController <UISearchBarDelegate> {
     NSString *_pdfPath;
-    NSArray *_results;
+    NSMutableArray *_results;
     UISearchBar *_searchBar;
+    UILabel *_progressLabel;
     CGPDFDocumentRef _document;
     id<SearchViewControllerDelegate> _delegate;
+    NSString *_activeTerm;
+    NSUInteger _searchPage;
+    NSUInteger _searchPageCount;
+    BOOL _searching;
+    BOOL _cancelRequested;
 }
 @property (nonatomic, assign) id<SearchViewControllerDelegate> delegate;
 - (id)initWithPDFPath:(NSString *)path;
