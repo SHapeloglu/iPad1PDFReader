6782700d9d1d665e5ddc4977bb04601299be5cd2
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 16:20:23 +0300

    Bound document navigator memory usage

--- MemoryBudget.h
+++ MemoryBudget.h
@@ -3,6 +3,8 @@
 #define IPAD1_THUMBNAIL_CACHE_LIMIT 8
 #define IPAD1_SEARCH_MAX_RESULTS 40
 #define IPAD1_REFLOW_PAGE_WINDOW 3
+#define IPAD1_NAVIGATOR_MAX_ITEMS 80
+#define IPAD1_NAVIGATOR_MAX_PER_KIND 40
 
 @interface MemoryBudget : NSObject
 + (void)logMemoryPolicy;
