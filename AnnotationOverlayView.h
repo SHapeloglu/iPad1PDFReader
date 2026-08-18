33e34f90383d3229a7cd31d83566483defdcf43a
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-18 16:15:13 +0300

    Add one-shot drag highlight selection

--- AnnotationOverlayView.h
+++ AnnotationOverlayView.h
@@ -1,7 +1,17 @@
 #import <UIKit/UIKit.h>
-@interface AnnotationOverlayView : UIView { NSString *_pdfPath; NSUInteger _page; BOOL _drawingEnabled; NSMutableArray *_points; }
+@interface AnnotationOverlayView : UIView {
+    NSString *_pdfPath;
+    NSUInteger _page;
+    BOOL _drawingEnabled;
+    BOOL _highlightSelectionEnabled;
+    NSMutableArray *_points;
+    CGPoint _highlightStart;
+    CGPoint _highlightCurrent;
+    BOOL _hasHighlightPreview;
+}
 @property(nonatomic,copy) NSString *pdfPath;
 @property(nonatomic,assign) NSUInteger page;
 @property(nonatomic,assign) BOOL drawingEnabled;
+@property(nonatomic,assign) BOOL highlightSelectionEnabled;
 - (void)reloadAnnotations;
 @end
