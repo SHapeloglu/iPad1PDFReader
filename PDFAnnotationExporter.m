c393b9ce19f7bbc3c9f31a435165e994496e6c02
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-22 14:09:01 +0300

    Keep legacy drawing export behavior while adding text highlight rects

--- PDFAnnotationExporter.m
+++ PDFAnnotationExporter.m
@@ -39,41 +39,13 @@ static void DrawHighlightAnnotation(CGContextRef c, NSDictionary *a, CGRect box)
 
 @implementation PDFAnnotationExporter
 + (BOOL)exportFlattenedPDFAtPath:(NSString *)path toPath:(NSString *)outPath {
-    CGPDFDocumentRef d=CGPDFDocumentCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path]);
-    if(!d)return NO;
-    CGContextRef c=CGPDFContextCreateWithURL((CFURLRef)[NSURL fileURLWithPath:outPath],NULL,NULL);
-    if(!c){CGPDFDocumentRelease(d);return NO;}
-
+    CGPDFDocumentRef d=CGPDFDocumentCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path]); if(!d)return NO;
+    CGContextRef c=CGPDFContextCreateWithURL((CFURLRef)[NSURL fileURLWithPath:outPath],NULL,NULL); if(!c){CGPDFDocumentRelease(d);return NO;}
     size_t count=CGPDFDocumentGetNumberOfPages(d);
-    for(size_t i=1;i<=count;i++){
-        CGPDFPageRef p=CGPDFDocumentGetPage(d,i);
-        CGRect box=CGPDFPageGetBoxRect(p,kCGPDFMediaBox);
-        CGPDFContextBeginPage(c,NULL);
-        CGContextDrawPDFPage(c,p);
-
-        NSArray *annotations=[AnnotationStore annotationsForPath:path page:i];
-        for(NSDictionary *a in annotations){
-            NSString *type=[a objectForKey:@"type"];
-            if([type isEqualToString:@"highlight"]){
-                DrawHighlightAnnotation(c,a,box);
-            } else if([type isEqualToString:@"draw"]){
-                NSArray *pts=[a objectForKey:@"points"];
-                CGContextSetRGBStrokeColor(c,0,0,1,.9);
-                CGContextSetLineWidth(c,2);
-                for(NSUInteger k=0;k<[pts count];k++){
-                    CGPoint pt=CGPointFromString([pts objectAtIndex:k]);
-                    CGFloat x=box.origin.x+pt.x*box.size.width;
-                    CGFloat y=box.origin.y+(1.0f-pt.y)*box.size.height;
-                    if(k==0)CGContextMoveToPoint(c,x,y);else CGContextAddLineToPoint(c,x,y);
-                }
-                CGContextStrokePath(c);
-            }
-        }
+    for(size_t i=1;i<=count;i++){ CGPDFPageRef p=CGPDFDocumentGetPage(d,i); CGRect box=CGPDFPageGetBoxRect(p,kCGPDFMediaBox); CGPDFContextBeginPage(c,NULL); CGContextDrawPDFPage(c,p);
+        for(NSDictionary *a in [AnnotationStore annotationsForPath:path page:i]){ NSString *type=[a objectForKey:@"type"]; if([type isEqualToString:@"highlight"]){DrawHighlightAnnotation(c,a,box);} else if([type isEqualToString:@"draw"]){NSArray *pts=[a objectForKey:@"points"];CGContextSetRGBStrokeColor(c,0,0,1,.9);CGContextSetLineWidth(c,2);for(NSUInteger k=0;k<[pts count];k++){CGPoint pt=CGPointFromString([pts objectAtIndex:k]);CGFloat x=pt.x*box.size.width,y=pt.y*box.size.height;if(k==0)CGContextMoveToPoint(c,x,y);else CGContextAddLineToPoint(c,x,y);}CGContextStrokePath(c);} }
         CGPDFContextEndPage(c);
     }
-    CGPDFContextClose(c);
-    CGContextRelease(c);
-    CGPDFDocumentRelease(d);
-    return YES;
+    CGPDFContextClose(c); CGContextRelease(c); CGPDFDocumentRelease(d); return YES;
 }
 @end
