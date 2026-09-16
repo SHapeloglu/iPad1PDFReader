80f41069d215e12ef8133420b0372bee27a6f0ba
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-09-16 10:55:18 +0300

    Add lightweight Markdown reading state

--- TextReaderViewController.h
+++ TextReaderViewController.h
@@ -2,13 +2,17 @@
 
 @interface TextReaderViewController : UIViewController <UIAlertViewDelegate, UIActionSheetDelegate> {
     NSString *_filePath;
+    NSString *_sourceText;
     UITextView *_textView;
     UIScrollView *_horizontalScrollView;
     UIToolbar *_toolbar;
+    UIBarButtonItem *_markdownButton;
     NSString *_searchTerm;
     NSRange _lastMatch;
     CGFloat _fontSize;
     BOOL _wrapEnabled;
+    BOOL _isMarkdown;
+    BOOL _markdownReadingMode;
     unsigned long long _fileSize;
 }
 
